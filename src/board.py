4321ae6eeecc4ac65ccdd707110cda415d083502
benzor <bracsko94@gmail.com>
2024-08-13 15:47:55 +0200

    Added more docstrings.

--- src/board.py
+++ src/board.py
@@ -4,14 +4,29 @@ from enum import Enum
 
 
 class GameState(Enum):
+    """Enum representing the state of the game."""
     INITIALIZED = 0
+    """Board has been set up, but game not started yet."""
     STARTED = 1
+    """Game in-progress."""
     WIN = 2
+    """Game has been won by the player."""
     LOSS = -1
+    """Game has been lost by the player."""
 
 
 class Board:
+    """
+    Class representing the game board.
+    """
     def __init__(self, number_of_rows: int, number_of_columns: int, number_of_mines: int):
+        """
+        Constructor method for the game board.
+        :param number_of_rows: Number of rows on the game board, must be an integer between 1 and 30 (inclusive).
+        :param number_of_columns: Number of columns on the game board, must be an integer between 1 and 30 (inclusive).
+        :param number_of_mines: Number of hidden mines scattered throughout the game board. Must be an integer
+        between 1 and N - 1, where N is the total number of fields on the board.
+        """
         if number_of_rows <= 0 or number_of_columns <= 0:
             raise ValueError("The board must have a positive number of rows and columns.")
         if number_of_rows > 30 or number_of_columns > 30:
@@ -28,6 +43,12 @@ class Board:
         self._elements: list[BoardElement] = self._set_up_board()
 
     def __getitem__(self, coordinates: int | tuple[int, int]):
+        """
+        Returns a BoardElement based on indices or coordinates
+        :param coordinates: Can be a single int or a tuple of two ints. In the former case, it specifies a board element
+        in a row-continuous manner, and in the latter case by zero-indexed (row, column) coordinates.
+        :return: Returns the board element to which the index or coordinates refer to.
+        """
         if isinstance(coordinates, int):
             return self.elements[coordinates]
         if isinstance(coordinates, tuple) and len(coordinates) == 1:
@@ -43,6 +64,10 @@ class Board:
         raise TypeError(f"Invalid argument. Expected int or tuple of two ints, got {type(coordinates)} instead.")
 
     def __str__(self):
+        """
+        :return: Returns a string representation of the board displaying elements as mines ('m') or their
+        proximity numbers (for empty fields).
+        """
         horizontal_separator = "*" * (self.number_of_columns * 4 + 1) + "\n"
         def get_elements_in_row(row_number: int):
             return (self[row_number, j] for j in range(0, self.number_of_columns))
@@ -55,22 +80,27 @@ class Board:
 
     @property
     def number_of_rows(self):
+        """The number of rows on the board. This attribute is immutable."""
         return self._number_of_rows
 
     @property
     def number_of_columns(self):
+        """The number of columns on the board. This attribute is immutable."""
         return self._number_of_columns
 
     @property
     def number_of_elements(self):
+        """The total number of fields on the board. This attribute is immutable."""
         return self._number_of_elements
 
     @property
     def number_of_mines(self):
+        """The total number of mines on the board. This attribute is immutable."""
         return self._number_of_mines
 
     @property
     def game_state(self):
+        """The state of the game. Can be INITIALIZED, STARTED, WIN or LOSS."""
         return self._game_state
 
     @game_state.setter
@@ -80,6 +110,7 @@ class Board:
 
     @property
     def elements(self):
+        """List of all the fields on the board. This attribute is immutable."""
         return self._elements
 
     def index_to_coordinates(self, idx: int):
@@ -93,12 +124,20 @@ class Board:
         return row * self.number_of_columns + col
 
     def swap_mine_with_empty_element(self, row: int, col: int):
+        """
+        Specify a board element. If it is a mine, it will be swapped with a random empty element.
+        :param row: Row index of the board element.
+        :param col: Column index of the board element.
+        """
         idx_mine = self.coordinates_to_index(row, col)
         idx_empty = sample([(idx, element) for (idx, element) in enumerate(self.elements) if not element.is_mine],
                            k=1)[0][0]
         self._elements[idx_mine], self._elements[idx_empty] = self._elements[idx_empty], self._elements[idx_mine]
 
     def calculate_proximities(self):
+        """
+        Computes the proximity numbers for all board elements (i.e. how many mines are nearby).
+        """
         empty_elements = [(idx, element) for (idx, element) in enumerate(self.elements) if not element.is_mine]
         for idx, element in empty_elements:
             row, col = self.index_to_coordinates(idx)
@@ -106,6 +145,9 @@ class Board:
             element.proximity = proximity
 
     def check_win_state(self):
+        """
+        Checks whether the game is in a win state and updates the game_state field accordingly.
+        """
         all_mines_flagged = all([element.state == BoardElementState.FLAGGED for element in self.elements if element.is_mine])
         all_non_mines_revealed = all([element.state == BoardElementState.REVEALED
                                      for element in self.elements if not element.is_mine])
@@ -113,6 +155,12 @@ class Board:
             self.game_state = GameState.WIN
 
     def reveal_element(self, row: int, col: int):
+        """
+        Reveals the specified board element if it is not already revealed. If the element has zero proximity number,
+        all neighbours will also be revealed in a recursive manner.
+        :param row: Row index of the board element to be revealed.
+        :param col: Column index of the board element to be revealed.
+        """
         element = self[row, col]
         if element.state == BoardElementState.REVEALED:
             return
