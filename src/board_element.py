db67d4b50455a2ae093fb4ee0397dcbea448618c
benzor <bracsko94@gmail.com>
2024-08-13 10:47:26 +0200

    Added docstrings for BoardElement.

--- src/board_element.py
+++ src/board_element.py
@@ -8,20 +8,33 @@ class BoardElementState(Enum):
 
 
 class BoardElement:
+    """
+    Represents a field in the Minesweeper board.
+    """
     def __init__(self, mined: bool):
+        """
+        Constructs a board element. The default state is HIDDEN, and with unassigned proximity (-1).
+        :param mined: Whether the field contains a mine or not.
+        """
         self._is_mine: bool = mined
         self._state = BoardElementState.HIDDEN
         self._proximity = -1
 
     def __str__(self):
+        """
+        String representation of a board element.
+        :return: Returns 'm' for mine and the proximity number (0..8) for an empty element.
+        """
         return "m" if self.is_mine else str(self.proximity)
 
     @property
     def is_mine(self):
+        """Whether the field is a mine or empty. This attribute is immutable."""
         return self._is_mine
 
     @property
     def state(self):
+        """The state of the field, which can be HIDDEN, FLAGGED or REVEALED."""
         return self._state
 
     @state.setter
@@ -30,6 +43,7 @@ class BoardElement:
 
     @property
     def proximity(self):
+        """The number of nearby fields with mines on them. Has a value of -1 before proper initialization and for mines."""
         return self._proximity
 
     @proximity.setter
